022389356dc295e44d88fe5e7deeec3c76434e69
Rob Graessle <rob.graessle@xilinx.com>
2023-10-07 04:57:02 -0400

    Removing Vitis Export example from Git

--- Examples/AIENGINE/Export_to_Vitis/vitis_workspace/mm2s/mm2s/hls/.autopilot/db/autopilot.rtl.models.tcl
+++
@@ -1,9 +0,0 @@
-set SynModuleInfo {
-  {SRCNAME mm2s MODELNAME mm2s RTLNAME mm2s IS_TOP 1
-    SUBMODULES {
-      {MODELNAME mm2s_gmem_m_axi RTLNAME mm2s_gmem_m_axi BINDTYPE interface TYPE adapter IMPL m_axi}
-      {MODELNAME mm2s_control_s_axi RTLNAME mm2s_control_s_axi BINDTYPE interface TYPE interface_s_axilite}
-      {MODELNAME mm2s_regslice_both RTLNAME mm2s_regslice_both BINDTYPE interface TYPE interface_regslice INSTNAME mm2s_regslice_both_U}
-    }
-  }
-}
