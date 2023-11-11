94183ee27930d50ab800eb185f8c7929b42b24a1
SrilakshmiDegala <141322480+SrilakshmiDegala@users.noreply.github.com>
2023-11-11 19:10:55 +0530

    window to buffer interface conversion

--- Examples/AIENGINE/Importing_AIE_blocks/AIE_Graph/src/kernels/window_in_window_out_fixed_size.cc
+++ Examples/AIENGINE/Importing_AIE_blocks/AIE_Graph/src/kernels/window_in_window_out_fixed_size.cc
@@ -14,16 +14,21 @@
 // limitations under the License.
 ///////////////////////////////////////////////////////////////////////////
 
-
 #include <adf.h>
+#include "aie_api/aie.hpp"
 #include "window_in_window_out_fixed_size.h"
-void add_kernel(input_window_int16 * inw1,input_window_int16 * inw2, output_window_int16 * outw)
+void add_kernel(adf::input_buffer<int16> & restrict inw1,adf::input_buffer<int16> & restrict inw2, adf::output_buffer<int16> &  restrict outw)
 {
-	int16 temp1,temp2,temp_out;
-	for (unsigned i=0; i<NUM_SAMPLES; i++) {
-		window_readincr(inw1,temp1);
-		window_readincr(inw2,temp2);
-		temp_out = temp1 + temp2;
-		window_writeincr(outw,temp_out);
-	}
+
+ 
+    auto outwt = aie::begin_vector<NUM_SAMPLES>( outw );
+    auto inw1t = aie::begin_vector<NUM_SAMPLES>( inw1 );
+    auto inw2t = aie::begin_vector<NUM_SAMPLES>( inw2 );
+    
+    aie::vector<int16, 16> temp1 = *inw1t++;
+    aie::vector<int16, 16> temp2 = *inw2t++;
+    aie::vector<int16, 16> temp_out= aie::add(temp1,temp2) ;
+ 
+           (*outwt++) = temp_out;
+
 }
