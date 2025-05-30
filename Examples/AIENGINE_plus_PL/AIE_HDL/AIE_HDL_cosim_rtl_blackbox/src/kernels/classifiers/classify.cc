2db3ecbc42195c7658e672cc9c4d97bd7bf21026
SrilakshmiDegala <141322480+SrilakshmiDegala@users.noreply.github.com>
2025-05-30 13:39:02 +0530

    updating classifier source code
    CR-1241646

--- Examples/AIENGINE_plus_PL/AIE_HDL/AIE_HDL_cosim_rtl_blackbox/src/kernels/classifiers/classify.cc
+++ Examples/AIENGINE_plus_PL/AIE_HDL/AIE_HDL_cosim_rtl_blackbox/src/kernels/classifiers/classify.cc
@@ -17,19 +17,20 @@
 
 #include <adf.h>
 #include "../../include.h"
+#include "../../kernels.h"
 
 
 // Nobody would build a classifier this way.  It's just to show the
 // sample by sample processing
-
+using namespace adf;
 
 void classifier
 (
     input_stream_cint16 * input,
-    output_window_int32 * outputw
+    output_buffer<int32> & __restrict outputw
 ) {
     const unsigned output_samples =  CLASSIFIER_OUTPUT_SAMPLES ;
-
+    auto OutIter = aie::begin(outputw);
 
     for (unsigned l=0;l<CLASSIFIER_OUTPUT_SAMPLES;l++) {
     #ifdef __chess__
@@ -42,15 +43,17 @@ void classifier
     #endif
       if (sample.real >= 0) {
 	if (sample.imag > 0)
-	  window_writeincr(outputw,0);
+	   *OutIter++ = 0;
         else
-          window_writeincr(outputw,1);
+	   *OutIter++ = 1;
       }
       else {
 	if (sample.imag > 0)
-	  window_writeincr(outputw,2);
+	   *OutIter++ = 2;
         else
-          window_writeincr(outputw,3);
+	   *OutIter++ = 3;
       }
     }
 }
+
+
