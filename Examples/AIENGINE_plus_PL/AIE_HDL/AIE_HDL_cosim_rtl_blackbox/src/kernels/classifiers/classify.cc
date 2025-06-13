38da777926b0ff766712b1dd4b0e8f3f7e3452f7
SrilakshmiDegala <141322480+SrilakshmiDegala@users.noreply.github.com>
2025-06-13 17:02:48 +0530

    Updating Classifier kernel code

--- Examples/AIENGINE_plus_PL/AIE_HDL/AIE_HDL_cosim_rtl_blackbox/src/kernels/classifiers/classify.cc
+++ Examples/AIENGINE_plus_PL/AIE_HDL/AIE_HDL_cosim_rtl_blackbox/src/kernels/classifiers/classify.cc
@@ -14,40 +14,39 @@
 // limitations under the License.
 ///////////////////////////////////////////////////////////////////////////
 
-
 #include <adf.h>
+#include <aie_api/aie.hpp>
 #include "../../include.h"
-#include "../../kernels.h"
 
-
-// Nobody would build a classifier this way.  It's just to show the
-// sample by sample processing
 using namespace adf;
 
 void classifier(input_stream_cint16 * input, 
-                output_buffer<int32> & __restrict output)
-{
+                output_buffer<int32> & __restrict output
+){
     
     const unsigned output_samples =  CLASSIFIER_OUTPUT_SAMPLES ;
-    auto OutIter = aie::begin(outputw);
-
-    for (unsigned l=0;l<CLASSIFIER_OUTPUT_SAMPLES;l++) {
-
+    auto OutIter = aie::begin(output);
+    for (unsigned l=0;l<output_samples;l++) {
       cint16 sample = readincr(input);
 
-      if (sample.real >= 0) {
-	    if (sample.imag > 0)
-	       *OutIter++ = 0;
-            else
-	       *OutIter++ = 1;
-          }
-          else {
-	    if (sample.imag > 0)
-	       *OutIter++ = 2;
-            else
-	       *OutIter++ = 3;
-          }
-        }
+    if (sample.real >= 0) {
+      if (sample.imag > 0)
+        //window_writeincr(outputw,0);
+        *OutIter++ = 0;
+        else
+          //window_writeincr(outputw,1);
+          *OutIter++ = 1;
+      }
+      else {
+        
+      if (sample.imag > 0)
+          //window_writeincr(outputw,2);
+          *OutIter++ = 2;
+        else
+          //window_writeincr(outputw,3);
+	  *OutIter++ = 3;
+      }
+    }
 }
 
 
