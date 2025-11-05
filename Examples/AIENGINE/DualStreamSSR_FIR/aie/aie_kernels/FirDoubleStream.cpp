dd458cc2d6d990fcdc51fb4e8b2eeaa84038383d
Rob Graessle <rob.graessle@xilinx.com>
2025-11-05 18:45:31 +0000

    Updating from stream to cascade interfaces

--- Examples/AIENGINE/DualStreamSSR_FIR/aie/aie_kernels/FirDoubleStream.cpp
+++ Examples/AIENGINE/DualStreamSSR_FIR/aie/aie_kernels/FirDoubleStream.cpp
@@ -1,25 +1,18 @@
 /*
- * (c) Copyright 2020 Xilinx, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+SPDX-License-Identifier: MIT
+*/
+
+
  #include <adf.h>
+#include <aie_api/aie.hpp>
+#include <aie_api/aie_adf.hpp>
+#include <aie_api/utils.hpp>
 
 #include "FirDoubleStream.h"
 
 template <int NSamples,int ShiftAcc,bool DiscardSample,bool SwapRead>
-void DoubleStream::FIR_MultiKernel_cout<NSamples,ShiftAcc,DiscardSample,SwapRead>::filter(input_stream_cint16* restrict sin1,input_stream_cint16* restrict sin2,output_stream_cacc48* cout)
+void DoubleStream::FIR_MultiKernel_cout<NSamples,ShiftAcc,DiscardSample,SwapRead>::filter(input_stream<cint16>* restrict sin1,input_stream<cint16>* restrict sin2,output_cascade<cacc48>* cout)
 {
 	v8cint16 taps =  *(v8cint16*) weights;
 	v16cint16 *ptr_delay_line = (v16cint16 *)delay_line;
@@ -49,11 +42,11 @@ void DoubleStream::FIR_MultiKernel_cout<NSamples,ShiftAcc,DiscardSample,SwapRead
 
 		acc = mul4(data,Start_1,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_3,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 		acc = mul4(data,Start_5,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_7,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 
 		if(SwapRead)
@@ -63,11 +56,11 @@ void DoubleStream::FIR_MultiKernel_cout<NSamples,ShiftAcc,DiscardSample,SwapRead
 
 		acc = mul4(data,Start_9,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_11,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 		acc = mul4(data,Start_13,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_15,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 	}
 
@@ -78,7 +71,7 @@ void DoubleStream::FIR_MultiKernel_cout<NSamples,ShiftAcc,DiscardSample,SwapRead
 
 
 template <int NSamples,int ShiftAcc,bool DiscardSample,bool SwapRead>
-void DoubleStream::FIR_MultiKernel_cincout<NSamples,ShiftAcc,DiscardSample,SwapRead>::filter(input_stream_cint16* restrict sin1,input_stream_cint16* restrict sin2,input_stream_cacc48* cin,output_stream_cacc48* cout)
+void DoubleStream::FIR_MultiKernel_cincout<NSamples,ShiftAcc,DiscardSample,SwapRead>::filter(input_stream<cint16>* restrict sin1,input_stream<cint16>* restrict sin2,input_cascade<cacc48>* cin,output_cascade<cacc48>* cout)
 {
 
 	v8cint16 taps =  *(v8cint16*) weights;
@@ -110,12 +103,12 @@ void DoubleStream::FIR_MultiKernel_cincout<NSamples,ShiftAcc,DiscardSample,SwapR
 
 		acc = mac4(acc,data,Start_1,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_3,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 		acc = readincr_v4(cin);
 		acc = mac4(acc,data,Start_5,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_7,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 
 		acc = readincr_v4(cin);
@@ -126,12 +119,12 @@ void DoubleStream::FIR_MultiKernel_cincout<NSamples,ShiftAcc,DiscardSample,SwapR
 
 		acc = mac4(acc,data,Start_9,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_11,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 
 		acc = readincr_v4(cin);
 		acc = mac4(acc,data,Start_13,0x3210,1,taps,0,0x0000,1);
 		acc = mac4(acc,data,Start_15,0x3210,1,taps,2,0x0000,1);
-		writeincr_v4(cout,acc);
+		writeincr(cout,acc);
 	}
 
 	*ptr_delay_line = data;
@@ -139,7 +132,7 @@ void DoubleStream::FIR_MultiKernel_cincout<NSamples,ShiftAcc,DiscardSample,SwapR
 
 
 template <int NSamples,int ShiftAcc,bool DiscardSample,bool SwapRead>
-void DoubleStream::FIR_MultiKernel_cin<NSamples,ShiftAcc,DiscardSample,SwapRead>::filter(input_stream_cint16* restrict sin1,input_stream_cint16* restrict sin2,input_stream_cacc48* cin,output_stream_cint16* restrict sout1,output_stream_cint16* restrict sout2)
+void DoubleStream::FIR_MultiKernel_cin<NSamples,ShiftAcc,DiscardSample,SwapRead>::filter(input_stream<cint16>* restrict sin1,input_stream<cint16>* restrict sin2,input_cascade<cacc48>* cin,output_stream<cint16>* restrict sout1,output_stream<cint16>* restrict sout2)
 {
 
 	v8cint16 taps =  *(v8cint16*) weights;
--- Examples/AIENGINE/DualStreamSSR_FIR/aie/aie_kernels/FirDoubleStream.h
+++ Examples/AIENGINE/DualStreamSSR_FIR/aie/aie_kernels/FirDoubleStream.h
@@ -1,19 +1,8 @@
 /*
- * (c) Copyright 2020 Xilinx, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+SPDX-License-Identifier: MIT
+*/
+
  #pragma once
 
 #include <adf.h>
@@ -34,7 +23,7 @@ public:
 		for(int i=0;i<16;i++) delay_line[i] = (cint16){0,0};
 	};
 
-	void filter(input_stream_cint16*  restrict sin1,input_stream_cint16*  restrict sin2,output_stream_cacc48*  cout);
+	void filter(input_stream<cint16>*  restrict sin1,input_stream<cint16>*  restrict sin2,output_cascade<cacc48>*  cout);
 
 	static void registerKernelClass()
 	{
@@ -56,7 +45,7 @@ public:
 		for(int i=0;i<16;i++) delay_line[i] = (cint16){0,0};
 	};
 
-	void filter(input_stream_cint16*  restrict sin1,input_stream_cint16*  restrict sin2,input_stream_cacc48*  cin,output_stream_cacc48*  cout);
+	void filter(input_stream<cint16>*  restrict sin1,input_stream<cint16>*  restrict sin2,input_cascade<cacc48>*  cin,output_cascade<cacc48>*  cout);
 
 	static void registerKernelClass()
 	{
@@ -78,7 +67,7 @@ public:
 		for(int i=0;i<16;i++) delay_line[i] = (cint16){0,0};
 	};
 
-	void filter(input_stream_cint16*  restrict sin1,input_stream_cint16*  restrict sin2,input_stream_cacc48*  cin,output_stream_cint16*  restrict sout1, output_stream_cint16*  restrict sout2);
+	void filter(input_stream<cint16>*  restrict sin1,input_stream<cint16>*  restrict sin2,input_cascade<cacc48>*  cin,output_stream<cint16>*  restrict sout1, output_stream<cint16>*  restrict sout2);
 
 	static void registerKernelClass()
 	{
